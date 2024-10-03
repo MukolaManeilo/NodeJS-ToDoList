d22375d1bdb1014e2c69ee275cf37ee874b5ced5
KolyaManeilo <kolamanejlo@gmail.com>
2024-10-03 20:24:28 +0300

    Add new file

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan')
+
 const app = express();
+
+app.set('view engine', 'ejs');
 
 const PORT = 3000;
 
-const createPath = (page) => Path2D.resolve(__dirname, 'views', page + '.html');
-
 app.listen(PORT, (error => {
-    error ? console.log(error) : console.log('https://localhost:' + PORT);
+    error ? console.log(error) : console.log('http://localhost:' + PORT);
 }));
 
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.ejs`);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 app.get('/', (req,res) => {
-    res.sendFile(createPath('index'));
+    const title = 'Home';
+    res.render(createPath('index'), {title});
 });
 
 app.get('/edit', (req,res) => {
-    res.sendFile(createPath('edit'));
-});
-
-app.post('/edit', (req,res) => {
-
+    const title = 'Edit';
+    res.render(createPath('edit'), {title});
 });
 
 
 app.get('/create', (req,res) => {
-    res.sendFile(createPath('edit'));
-});
-
-app.post('/create', (req,res) => {
-
+    const title = 'Create';
+    res.render(createPath('create'), {title});
 });
 
 app.use((req, res) => {
     res
         .status(404)
-        .sendFile(createPath('error'));
+        .render(createPath('error'));
 })
