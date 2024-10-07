47a1c90e4b1324821de595a1efb69966e2f51903
KolyaManeilo <kolamanejlo@gmail.com>
2024-10-07 05:58:37 +0300

    Switch to MVC

---
+++ controllers/task-controller.js
@@ -0,0 +1,110 @@
+const Task = require('../models/task');
+const createPath = require('../helpers/create-path');
+
+const getTask = (req, res) => {
+    const pageTitle = 'Home';
+
+    Task.find()
+        .then(tasks => {
+            res.render(createPath('index'), { pageTitle, tasks });
+        })
+        .catch(err => {
+            console.error('Error fetching tasks:', err);
+            res.status(500).send('Error fetching tasks');
+        });
+};
+
+const getCreateTask = (req, res) => {
+    const pageTitle = 'Create';
+    res.render(createPath('create'), { pageTitle });
+};
+
+const postCreateTask = (req, res) => {
+    const { title, type, desc } = req.body;
+    const task = new Task({ title, type, desc, status: false });
+
+    task.save()
+        .then(() => res.redirect('/'))
+        .catch(error => {
+            console.error('Error saving task:', error);
+            res.status(500).send('Error creating task');
+        });
+};
+
+const getEditTask = (req, res) => {
+    const taskId = req.params.id;
+    const pageTitle = 'Edit';
+
+    Task.findById(taskId)
+        .then(task => {
+            if (!task) {
+                return res.status(404).send('Task not found');
+            }
+            res.render(createPath('edit'), { pageTitle, task });
+        })
+        .catch(error => {
+            console.error('Error fetching task:', error);
+            res.status(500).send('Error fetching task');
+        });
+};
+
+const postEditTask = (req, res) => {
+    const taskId = req.params.id;
+    const { title, type, desc } = req.body;
+
+    Task.findByIdAndUpdate(taskId, { title, type, desc }, { new: true })
+        .then(updatedTask => {
+            if (!updatedTask) {
+                return res.status(404).send('Task not found');
+            }
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.error('Error updating task:', error);
+            res.status(500).send('Error updating task');
+        });
+};
+
+const ChangTaskStatus = (req, res) => {
+    const taskId = req.params.id;
+
+    Task.findById(taskId)
+        .then(task => {
+            if (!task) {
+                return res.status(404).send('Task not found');
+            }
+
+            task.status = !task.status;
+
+            return task.save();
+        })
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.error('Error changing task status:', error);
+            res.status(500).send('Error changing task status');
+        });
+};
+
+const deleteTask = (req, res) => {
+    const taskId = req.params.id;
+
+    Task.findByIdAndDelete(taskId)
+        .then(() => res.redirect('/'))
+        .catch(error => {
+            console.error('Error deleting task:', error);
+            res.status(500).send('Error deleting task');
+        });
+};
+
+
+module.exports = {
+    getTask,
+    getCreateTask,
+    postCreateTask,
+    getEditTask,
+    postEditTask,
+    ChangTaskStatus,
+    deleteTask
+}
--- routes/task-routes.js
+++ routes/task-routes.js
@@ -1,108 +1,23 @@
 const express = require('express');
-const Task = require('../models/tasks');
+const Task = require('../models/task');
 const createPath = require('../helpers/create-path');
 const router = express.Router();
+const { getTask, getCreateTask, postCreateTask, getEditTask, postEditTask, ChangTaskStatus, deleteTask } 
+= require('../controllers/task-controller');
 
-// ------------------------/index-------------------------------------
-router.get('/', (req, res) => {
-    const pageTitle = 'Home';
 
-    Task.find()
-        .then(tasks => {
-            res.render(createPath('index'), { pageTitle, tasks });
-        })
-        .catch(err => {
-            console.error('Error fetching tasks:', err);
-            res.status(500).send('Error fetching tasks');
-        });
-});
 
-// ------------------------/create------------------------------------
-router.get('/task/create', (req, res) => {
-    const pageTitle = 'Create';
-    res.render(createPath('create'), { pageTitle });
-});
 
-router.post('/task/create', (req, res) => {
-    const { title, type, desc } = req.body;
-    const task = new Task({ title, type, desc, status: false });
+router.get('/', getTask);
 
-    task.save()
-        .then(() => res.redirect('/'))
-        .catch(error => {
-            console.error('Error saving task:', error);
-            res.status(500).send('Error creating task');
-        });
-});
+router.get('/task/create', getCreateTask);
+router.post('/task/create', postCreateTask);
 
-// ------------------------/edit-------------------------------------
-router.get('/task/edit/:id', (req, res) => {
-    const taskId = req.params.id;
-    const pageTitle = 'Edit';
+router.get('/task/edit/:id', getEditTask);
+router.post('/task/edit/:id', postEditTask);
 
-    Task.findById(taskId)
-        .then(task => {
-            if (!task) {
-                return res.status(404).send('Task not found');
-            }
-            res.render(createPath('edit'), { pageTitle, task });
-        })
-        .catch(error => {
-            console.error('Error fetching task:', error);
-            res.status(500).send('Error fetching task');
-        });
-});
+router.get('/task/changestatus/:id', ChangTaskStatus);
 
-router.post('/task/edit/:id', (req, res) => {
-    const taskId = req.params.id;
-    const { title, type, desc } = req.body;
-
-    Task.findByIdAndUpdate(taskId, { title, type, desc }, { new: true })
-        .then(updatedTask => {
-            if (!updatedTask) {
-                return res.status(404).send('Task not found');
-            }
-            res.redirect('/');
-        })
-        .catch(error => {
-            console.error('Error updating task:', error);
-            res.status(500).send('Error updating task');
-        });
-});
-
-// ------------------------/changeStatus--------------------------------
-router.get('/task/changestatus/:id', (req, res) => {
-    const taskId = req.params.id;
-
-    Task.findById(taskId)
-        .then(task => {
-            if (!task) {
-                return res.status(404).send('Task not found');
-            }
-
-            task.status = !task.status;
-
-            return task.save();
-        })
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch(error => {
-            console.error('Error changing task status:', error);
-            res.status(500).send('Error changing task status');
-        });
-});
-
-// ------------------------/delete--------------------------------------
-router.get('/task/delete/:id', (req, res) => {
-    const taskId = req.params.id;
-
-    Task.findByIdAndDelete(taskId)
-        .then(() => res.redirect('/'))
-        .catch(error => {
-            console.error('Error deleting task:', error);
-            res.status(500).send('Error deleting task');
-        });
-});
+router.get('/task/delete/:id', deleteTask);
 
 module.exports = router;
